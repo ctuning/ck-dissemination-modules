029f450f889a85a2eb998bdd1e5579e01dcc7392
Grigori Fursin <grigori.fursin@cTuning.org>
2017-04-07 12:02:01 +0200

    added module:paper

--- module/slide/module.py
+++ module/slide/module.py
@@ -38,8 +38,8 @@ def prepare_for_latex(i):
     Input:  {
               data_uoa             - slide entry
               file                 - slide file to process (in pdf)
-              (crop)               - if 'yes', crop 
               (uid)                - env UID to substitute, if empty use filebase
+              (extra_uid)          - append to the beginning of the output file
               (add_braces_to_file) - if 'yes', add {} to filename before extension (to allow . in filename)
             }
 
@@ -54,12 +54,14 @@ def prepare_for_latex(i):
     """
 
     import os
+    import filecmp
+    import shutil
 
     duoa=i['data_uoa']
     muoa=i['module_uoa']
     f=i['file']
 
-    crop=i.get('crop','')
+    path=i.get('path','')
 
     # Get path to entry
     r=ck.access({'action':'load',
@@ -71,34 +73,34 @@ def prepare_for_latex(i):
     # Check file
     pp=os.path.join(p,f)
 
+    if not os.path.isfile(pp):
+       return {'return':1, 'error':'file "'+pp+'" not found'}
+
     uid=i.get('uid','')
     if uid=='':
        uid=os.path.splitext(f)[0]
 
-    # Check crop
-    if crop=='yes':
-       fn,fe=os.path.splitext(f)
-       fc=fn+'-cropped'+fe
-       fcpng=fn+'-cropped.png'
+    # New path
+    ppn=os.getcwd()
 
-       ppc=os.path.join(p,fc)
-       ppcpng=os.path.join(p,fcpng)
+    if path!='':
+       ppn=os.path.join(ppn,path)
 
-       if os.path.isfile(ppc):
-          os.remove(ppc)
+    ppd=ppn
 
-       # Convert to cropped pdf
-       os.system('pdfcrop '+pp+' '+ppc)
+    euid=i.get('extra_uid','')
+    if euid!='':
+       f=euid+'.'+f
 
-       # Check
-       if not os.path.isfile(ppc):
-          return {'return':1, 'error':'cropped file was not created'}
+    ppn=os.path.join(ppn,f)
 
-       # Convert to cropped png
-       os.system('convert -density 300 -quality 100 -scale 800 '+ppc+' '+ppcpng)
+    # Check if files are not the same
+    if not os.path.isfile(ppn) or not filecmp.cmp(pp,ppn):
+       if not os.path.isdir(ppd):
+          os.makedirs(ppd)
 
-       pp=ppc
-       f=fc
+       ck.out('Copying file '+f+' to '+path+' ...')
+       shutil.copyfile(pp,ppn)
 
     # Check braces
     if i.get('add_braces_to_file','')=='yes':
@@ -111,3 +113,91 @@ def prepare_for_latex(i):
     sub['CK_PATH_'+uid]=pp.replace('\\','/') # convert path to Unix format
 
     return {'return':0, 'substitute':sub}
+
+##############################################################################
+# convert to various format
+
+def convert(i):
+    """
+    Input:  {
+               data_uoa             - slide entry
+               (file)               - file to convert
+
+               (density)            - png density
+               (quality)            - png quality
+               (scale)              - png scale
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    import os
+
+    duoa=i['data_uoa']
+    muoa=i['module_uoa']
+ 
+    # Get path to an entry
+    r=ck.access({'action':'load',
+                 'module_uoa':muoa,
+                 'data_uoa':duoa})
+    if r['return']>0: return r
+    d=r['dict']
+    p=r['path']
+
+    pd=i.get('density','')
+    if pd=='': pd='300'
+    pq=i.get('quality','')
+    if pq=='': pq='100'
+    ps=i.get('scale','')
+    if ps=='': ps='800'
+
+    slides=d.get('slides',[])
+
+    f=i.get('file','')
+    if f!='':
+       f=os.path.splitext(f)[0]
+       slides=[f]
+
+    # Processing slides
+    for fn in slides:
+        ck.out('==================================================================================')
+        ck.out('* '+fn)
+        ck.out('')
+
+        f=fn+'.pdf'
+        pp=os.path.join(p,f)
+
+        fc=fn+'-cropped.pdf'
+        fcpng=fn+'-cropped.png'
+
+        ppc=os.path.join(p,fc)
+        ppcpng=os.path.join(p,fcpng)
+
+        if os.path.isfile(ppc):
+           os.remove(ppc)
+
+        # Convert to cropped pdf
+        s='pdfcrop '+pp+' '+ppc
+        ck.out(s)
+        os.system(s)
+
+        # Check
+        if not os.path.isfile(ppc):
+           return {'return':1, 'error':'cropped file was not created'}
+
+        # Convert to cropped png
+        s='convert -density '+pd+' -quality '+pq+' -scale '+ps+' '+ppc+' '+ppcpng
+        ck.out('')
+        ck.out(s)
+        os.system(s)
+
+        # Check
+        if not os.path.isfile(ppcpng):
+           return {'return':1, 'error':'PNG file was not created'}
+
+    return {'return':0}
