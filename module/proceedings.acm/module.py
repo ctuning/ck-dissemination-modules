1d7f2ef1d82f2c6edf7c0bca812930440453d956
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-06-02 17:32:09 +0200

    added module proceedings.acm with action "generate" to automate generation of proceedings (such as ReQuEST, http://cKnowledge.org/request) with papers and artifacts in the CK format

---
+++ module/proceedings.acm/module.py
@@ -0,0 +1,51 @@
+#
+# Collective Knowledge (automating ACM proceedings with papers and artifacts)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+cfg={}  # Will be updated by CK (meta description of this module)
+work={} # Will be updated by CK (temporal data)
+ck=None # Will be updated by CK (initialized CK kernel) 
+
+# Local settings
+
+##############################################################################
+# Initialize module
+
+def init(i):
+    """
+
+    Input:  {}
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    return {'return':0}
+
+##############################################################################
+# generate ACM proceedings
+
+def generate(i):
+    """
+    Input:  {
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    # TBD
+
+    return {'return':0}
